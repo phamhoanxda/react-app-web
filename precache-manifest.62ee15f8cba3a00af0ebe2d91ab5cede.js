b5c8c1ea821c3a33887b511b588ed091d2a3068a
phamhoan_nuce <68221210+phamhoanxda@users.noreply.github.com>
2020-09-25 12:37:33 +0700

    add 2509

---
+++ precache-manifest.62ee15f8cba3a00af0ebe2d91ab5cede.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbeec16c00ded2c30377a2ae1ad79e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44f78ae4749a44285c8e",
+    "url": "/static/css/2.85b71462.chunk.css"
+  },
+  {
+    "revision": "5cb19ff9689b69ef59f1",
+    "url": "/static/css/3.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a4b638087767eef552cd",
+    "url": "/static/css/4.0a88d653.chunk.css"
+  },
+  {
+    "revision": "6de4166a4c825696df40",
+    "url": "/static/css/main.3d457077.chunk.css"
+  },
+  {
+    "revision": "44f78ae4749a44285c8e",
+    "url": "/static/js/2.a1ac03dd.chunk.js"
+  },
+  {
+    "revision": "77548c0d66ac42292abfda2c9c306920",
+    "url": "/static/js/2.a1ac03dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb19ff9689b69ef59f1",
+    "url": "/static/js/3.a135738e.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/static/js/3.a135738e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b638087767eef552cd",
+    "url": "/static/js/4.38141983.chunk.js"
+  },
+  {
+    "revision": "6de4166a4c825696df40",
+    "url": "/static/js/main.a3a8fae7.chunk.js"
+  },
+  {
+    "revision": "cc6b90e99b6f1ed41e4f",
+    "url": "/static/js/runtime-main.2789d0db.js"
+  },
+  {
+    "revision": "146c36c8ff0d0879ac2151a8c5d01dbc",
+    "url": "/static/media/blue-bg.146c36c8.jpg"
+  },
+  {
+    "revision": "cb9ae265b9fad5e6f9d382d1344b3269",
+    "url": "/static/media/colorful-bg.cb9ae265.jpg"
+  },
+  {
+    "revision": "e6624e7ee3d44d0011cd9c032d126b8d",
+    "url": "/static/media/orange-bg.e6624e7e.jpg"
+  }
+]);
